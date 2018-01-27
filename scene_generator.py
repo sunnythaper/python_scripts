342d461b5fab4e3ed3739d8d3509b428671272f7
Sunny Thaper <sthaper@gmail.com>
2018-01-26 19:34:21 -0700

    Added multiple domain support

--- scene_generator.py
+++ scene_generator.py
@@ -1,49 +1,62 @@
 # Pass the following:
 #
 # {
-#     "domain": "light",
+#     "domains": ["light", "switch"],
 #     "attributes": ["brightness", "color_temp", "xy_color", "rgb_color"],
 #     "save_file": true
 # }
 
 # SETUP VARIABLES FROM HASS CALL
-domain = data.get('domain')
+domains = data.get('domains')
 attributes = data.get('attributes')
 save_file = data.get('save_file')
 
-# PRINT NICELY FORMATTED HEADER
-text = "\n\n"
-text = text + "#############################################\n"
-text = text + "## " + domain + "\n"
-text = text + "#############################################\n\n"
+# ACCOUNT FOR DEPRECIATED SETTINGS
+if data.get('domain'):
+    domains = [data.get('domain')]
 
-# FIND ALL ENTITIES BY DOMAIN
-entities = hass.states.entity_ids(domain)
+# SET YAML RECEIVER VARIABLE
+text = ""
 
-# GET ENTITY STATE & ATTRIBUTES
-for i in entities:
+# PROCESS EACH DOMAIN
+for i in domains:
 
-    # GET ENTITY DATA FROM HASS
-    entity = ("%r" % i).strip("'")
-    status = hass.states.get(entity)
+    # GET DOMAIN
+    domain = ("%r" % i).strip("'")
 
-    # ENTITY STATE
-    text = text + entity + ":\n"
-    text = text + "  state: " + status.state + "\n"
-
-    # ENTITY ATTRIBUTES WHEN STATE IS ON
-    if status.state == 'on':
-        for i in attributes:
-
-            # GET ATTRIBUTE BY JSON ARRAY PASSED BY HASS CALL
-            attributeState = ("%r" % i).strip("'")
-
-            # DISPLAY ATTRIBUTE IF NOT EMPTY
-            if status.attributes.get(attributeState):
-                text = text + "  " + attributeState + ": " + str(status.attributes.get(attributeState)) + "\n"
-
-    # GIVE THE PO DECLARATION SOME SPACE
+    # PRINT NICELY FORMATTED HEADER
     text = text + "\n"
+    text = text + "#############################################\n"
+    text = text + "## " + domain + "\n"
+    text = text + "#############################################\n\n"
+
+    # FIND ALL ENTITIES BY DOMAIN
+    entities = hass.states.entity_ids(domain)
+
+    # GET ENTITY STATE & ATTRIBUTES
+    for i in entities:
+
+        # GET ENTITY DATA FROM HASS
+        entity = ("%r" % i).strip("'")
+        status = hass.states.get(entity)
+
+        # ENTITY STATE
+        text = text + entity + ":\n"
+        text = text + "  state: " + status.state + "\n"
+
+        # ENTITY ATTRIBUTES WHEN STATE IS ON
+        if status.state == 'on':
+            for i in attributes:
+
+                # GET ATTRIBUTE BY JSON ARRAY PASSED BY HASS CALL
+                attributeState = ("%r" % i).strip("'")
+
+                # DISPLAY ATTRIBUTE IF NOT EMPTY
+                if status.attributes.get(attributeState):
+                    text = text + "  " + attributeState + ": " + str(status.attributes.get(attributeState)) + "\n"
+
+        # GIVE THE PO DECLARATION SOME SPACE
+        text = text + "\n"
 
 if save_file:
     # SAVE SCENE CONFIGURATION TO FILE
